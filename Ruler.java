7a8bc3789d2c26c139f68684b0e56e3a6bbe5a5f
jhinton2015 <jhint2011@gmail.com>
2014-10-28 14:37:06 -0400

    Ruler tool
    java and XML file

---
+++ Ruler.java
@@ -0,0 +1,18 @@
+package com.example.unitconverter;
+
+import android.os.Bundle;
+
+public class Ruler {
+//code to display the measurements to be done in next iteration
+	public void onCreate(Bundle savedInstanceState) {
+		// Required call through to Activity.onCreate()
+		// Restore any saved instance state
+		//super.onCreate(savedInstanceState);
+		setContentView(R.layout.rulertool);
+	}
+
+	private void setContentView(int convertertool) {
+		// TODO Auto-generated method stub
+		
+	}
+	}
